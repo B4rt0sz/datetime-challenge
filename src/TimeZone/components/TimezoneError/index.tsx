f87dc06919920b514be66da1c9ae24cbe3598488
B4rt0sz <bart8.developer@gmail.com>
2023-06-26 18:06:02 +0200

    update error message

--- src/TimeZone/components/TimezoneError/index.tsx
+++ src/TimeZone/components/TimezoneError/index.tsx
@@ -5,7 +5,7 @@ const TimezoneError = ({ error, resetErrorBoundary }: FallbackProps) => {
   return (
     <div className='dateTime__error'>
       <p>
-        Something went wrong with the API call: <span>{error.message}</span>
+        Something went wrong: <span>{error.message}</span>
       </p>
       <button onClick={resetErrorBoundary}>Reload aplication</button>
     </div>
